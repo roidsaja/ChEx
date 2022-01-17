74a05f934a2bbdd3830a2eb27089903c7f195a6e
roidsaja <roid0520@gmail.com>
2022-01-18 02:38:16 +0300

    style: footer styling

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,8 +9,26 @@
 	<slot />
 </main>
 
-<footer>
-	<p>Made by Roid using <a href="https://kit.svelte.dev">SvelteKit</a></p>
+<footer class="space-x-2">
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="16"
+		height="16"
+		fill="currentColor"
+		class="bi bi-code-slash"
+		viewBox="0 0 16 16"
+	>
+		<path
+			d="M10.478 1.647a.5.5 0 1 0-.956-.294l-4 13a.5.5 0 0 0 
+			.956.294l4-13zM4.854 4.146a.5.5 0 0 1 0 
+			.708L1.707 8l3.147 3.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 
+			.708 0zm6.292 0a.5.5 0 0 0 0 .708L14.293 8l-3.147 3.146a.5.5 0 0 0 
+			.708.708l3.5-3.5a.5.5 0 0 0 0-.708l-3.5-3.5a.5.5 0 0 0-.708 0z"
+		/>
+	</svg>
+	<p>
+		Made by Roid using <a href="https://kit.svelte.dev">SvelteKit</a>
+	</p>
 </footer>
 
 <style>
@@ -27,7 +45,7 @@
 
 	footer {
 		display: flex;
-		flex-direction: column;
+		flex-direction: row;
 		justify-content: center;
 		align-items: center;
 		padding: 40px;
@@ -35,6 +53,7 @@
 
 	footer a {
 		font-weight: bold;
+		color: orange;
 	}
 
 	@media (min-width: 480px) {
