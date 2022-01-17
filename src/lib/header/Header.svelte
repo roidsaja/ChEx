d504ac9f28a3cd87025a72173df01e4ac97266dc
roidsaja <roid0520@gmail.com>
2022-01-18 01:27:51 +0300

    style: pokedex index cards

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -4,16 +4,10 @@
 
 <header>
 	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
 		<ul>
 			<li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
 			<li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
 		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
 	</nav>
 </header>
 
@@ -27,16 +21,6 @@
 		display: flex;
 		justify-content: center;
 		--background: rgba(255, 255, 255, 0.7);
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
 	}
 
 	ul {
@@ -59,6 +43,7 @@
 
 	li.active::before {
 		--size: 6px;
+		--accent-color: #8134c9;
 		content: '';
 		width: 0;
 		height: 0;
@@ -70,6 +55,7 @@
 	}
 
 	nav a {
+		--heading-color: #8134c9;
 		display: flex;
 		height: 100%;
 		align-items: center;
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -10,7 +10,7 @@
 </main>
 
 <footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	<p>Made by Roid using <a href="https://kit.svelte.dev">SvelteKit</a></p>
 </footer>
 
 <style>
